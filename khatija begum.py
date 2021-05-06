dcf1c1eea46407c17257f312f7c239bacdb23b2b
khatija786 <83759450+khatija786@users.noreply.github.com>
2021-05-06 16:18:42 +0530

    Added python code

---
+++ khatija begum.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+print("hello world")
+
+
+# In[13]:
+
+
+x=3
+y=5
+
+
+# In[15]:
+
+
+x+y
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+name='fatima'
+
+
+# In[18]:
+
+
+print(name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+address='shaheen nagar'
+
+
+# In[20]:
+
+
+print(address)
+
+
+# In[22]:
+
+
+sweethouse=45
+
+
+# In[23]:
+
+
+print(sweethouse)
+
+
+# In[26]:
+
+
+name1='abc'
+
+
+# In[27]:
+
+
+print(name1)
+
+
+# In[28]:
+
+
+1name='bkh'
+
+
+# 
+
+# In[29]:
+
+
+name1='bkh'
+print(name1)
+
+
+# In[30]:
+
+
+name@='dhf'
+
+
+# In[31]:
+
+
+name1='khatija'
+print(name1)
+
+
+# In[32]:
+
+
+name2="khatija"
+print(name2)
+
+
+# In[33]:
+
+
+name3="""khatija"""
+print(name3)
+
+
+# In[34]:
+
+
+type(name1)
+
+
+# In[35]:
+
+
+type(name2)
+
+
+# In[36]:
+
+
+type(name3)
+
+
+# In[37]:
+
+
+type(sweethouse)
+
+
+# In[ ]:
+
+
+
+
